4f43ec5dcac5b5b54fbc5c37fe8434ac37c2b36f
Levan Roinishvili <levanroinishvili@gmail.com>
2023-12-21 10:47:31 +0000

    Added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [ ];
+export const routes: Routes = [
+    {
+        path: 'change-detection',
+        title: 'Change Detection',
+        loadComponent: () => import('./cd-root/cd-root.component').then(m => m.CdRootComponent),
+    },
+    {
+        path: 'joke',
+        title: 'Joke',
+        loadComponent: () => import('./joke/joke.component').then(m => m.JokeComponent),
+    }
+];
